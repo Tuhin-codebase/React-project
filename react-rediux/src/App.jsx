6d65e73bce356d1e7116ccef6530b34837f1d947
Tuhin-codebase <tuhinislamofficial.x@gmail.com>
2025-09-17 10:57:23 +0600

    how to add bootstarp in react-project

---
+++ react-rediux/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <button type="button" className="btn btn-primary">
+        Click Me
+      </button>
+      <button type="button" className="btn btn-secondary">
+        Hello sir{" "}
+      </button>
+    </>
+  );
+}
+
+export default App;
